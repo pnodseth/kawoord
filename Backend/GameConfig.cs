1cdc72419b432b70c5c9f94091d098c336412e31
Pål Nødseth <pal.nodseth@eggsdesign.com>
2022-01-15 00:05:35 +0100

    Add some validation to GameConfig constructor

--- Backend/GameConfig.cs
+++ Backend/GameConfig.cs
@@ -4,6 +4,26 @@ public class GameConfig
 {
     public GameConfig(int maxPlayers, int wordLength, int numberOfRounds, bool allowRandomPlayers, int roundLength, Language language)
     {
+        if (numberOfRounds > 8 | numberOfRounds <= 2)
+        {
+            throw new ArgumentOutOfRangeException("Number of rounds must be  between 2 and 8");
+        }
+
+        if (wordLength < 3 | wordLength > 8)
+        {
+            throw new ArgumentOutOfRangeException("wordLength must be between 3 and 8 characters");
+        }
+
+        if (maxPlayers < 2 | maxPlayers > 10)
+        {
+            throw new ArgumentOutOfRangeException("maxPlayers must be between 2 and 10");
+        }
+
+        if (roundLength < 30 | roundLength > 240)
+        {
+            throw new ArgumentOutOfRangeException("roundLength must be between 30 and 240 seconds");
+        }
+        
         MaxPlayers = maxPlayers;
         WordLength = wordLength;
         NumberOfRounds = numberOfRounds;
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,15 @@
+using Backend;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+
+app.MapPost("/game/new", () =>
+{
+    var config = new GameConfig(5, 5, 5, true, 120, Language.English);
+    var game = new Game(config);
+});
 
 app.Run();
