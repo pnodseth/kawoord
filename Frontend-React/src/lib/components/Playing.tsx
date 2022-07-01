60382279c992fe4e7ebbf7abcb78177bd9d5240f
Pål Nødseth <p@nodseth.no>
2022-07-01 22:24:56 +0200

    add framer-motion

--- Frontend-React/src/lib/components/Playing.tsx
+++ Frontend-React/src/lib/components/Playing.tsx
@@ -10,6 +10,7 @@ import { PlayerSubmittedView } from "$lib/components/PlayerSubmittedView";
 import { SyncLoader } from "react-spinners";
 import { RoundViewEnum } from "$lib/components/constants";
 import { PreRoundView } from "$lib/components/PreRoundView";
+import { motion } from "framer-motion";
 
 interface PlayingProps {
   player: Player;
@@ -73,48 +74,50 @@ export function Playing({ gameState, player }: PlayingProps) {
     }
   }
 
-  if (!gameState.game) return null;
-
   if (currentRound?.roundViewEnum === RoundViewEnum.Playing) {
     return (
-      <div className="playing-wrapper bg-white rounded  h-[70vh] text-gray-600 text-center flex flex-col pb-2">
-        <div className="px-8  pt-4">
-          <RoundViewHeader
-            game={gameState.game}
-            currentRound={currentRound}
-            letterArr={letterArr}
-            playerLetterHints={currentPlayerLetterHints}
-          />
-        </div>
-        {!playerHasSubmitted && (
-          <div className="px-8" style={{ userSelect: "none" }}>
-            <InputGrid invalidWord={invalidWord} letterArr={letterArr} correctLetters={correctLetterHints || []} />
-            <div className="spacer h-4"></div>
-          </div>
-        )}
-        {!playerHasSubmitted ? (
-          <>
-            <div className="keyboard-container px-2">
-              <Keyboard
-                letterHints={allLetterHints}
-                handleSubmit={handleSubmit}
+      <motion.div animate={{ scale: [0, 1] }} transition={{ duration: 0.2 }}>
+        <div className="playing-wrapper bg-white rounded  h-[70vh] text-gray-600 text-center flex flex-col pb-2">
+          <div className="px-8  pt-4">
+            {gameState.game && (
+              <RoundViewHeader
+                game={gameState.game}
+                currentRound={currentRound}
                 letterArr={letterArr}
-                setLetterArr={setLetterArr}
-                letterIdx={letterIdx}
-                setLetterIdx={setLetterIdx}
+                playerLetterHints={currentPlayerLetterHints}
               />
+            )}
+          </div>
+          {!playerHasSubmitted && (
+            <div className="px-8" style={{ userSelect: "none" }}>
+              <InputGrid invalidWord={invalidWord} letterArr={letterArr} correctLetters={correctLetterHints || []} />
+              <div className="spacer h-4"></div>
             </div>
-            <div className="spacer  h-1 sm:h-8 md:h8 mt-auto" />
-            <div>
-              <Button onClick={() => handleSubmit(letterArr.join(""))} disabled={submitting || invalidWord}>
-                {!submitting ? "Submit" : <SyncLoader color="#FFF" />}
-              </Button>
-            </div>
-          </>
-        ) : (
-          <PlayerSubmittedView submittedWord={submittedWord} />
-        )}
-      </div>
+          )}
+          {!playerHasSubmitted ? (
+            <>
+              <div className="keyboard-container px-2">
+                <Keyboard
+                  letterHints={allLetterHints}
+                  handleSubmit={handleSubmit}
+                  letterArr={letterArr}
+                  setLetterArr={setLetterArr}
+                  letterIdx={letterIdx}
+                  setLetterIdx={setLetterIdx}
+                />
+              </div>
+              <div className="spacer  h-1 sm:h-8 md:h8 mt-auto" />
+              <div>
+                <Button onClick={() => handleSubmit(letterArr.join(""))} disabled={submitting || invalidWord}>
+                  {!submitting ? "Submit" : <SyncLoader color="#FFF" />}
+                </Button>
+              </div>
+            </>
+          ) : (
+            <PlayerSubmittedView submittedWord={submittedWord} />
+          )}
+        </div>
+      </motion.div>
     );
   } else if (currentRound?.roundViewEnum === RoundViewEnum.NotStarted) {
     return <PreRoundView round={currentRound} />;
