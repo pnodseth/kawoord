a256be75683d4850604199f0a4901f8a4a700833
Pål Nødseth <p@nodseth.no>
2022-07-08 23:47:42 +0200

    style

--- Frontend-React/src/lib/components/PlayerSubmittedView.tsx
+++ Frontend-React/src/lib/components/PlayerSubmittedView.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { ClimbingBoxLoader } from "react-spinners";
 import useWordDefinition from "$lib/hooks/useWordDefinition";
+import FixedBottomContent from "$lib/layout/FixedBottomContent";
 
 interface PlayerHasSubmittedProps {
   submittedWord: string;
@@ -28,17 +29,19 @@ export const PlayerSubmittedView = ({ submittedWord }: PlayerHasSubmittedProps) 
       <h2 className="font-kawoord text-2xl">Great job!</h2>
       <div className="spacer h-6"></div>
       <h2 className="font-kawoord text-xl">You submitted: {submittedWord.toUpperCase()}</h2>
-      <div className="spacer h-10 xl:h-20"></div>
+      <div className="spacer sm:8 xl:h-20"></div>
       <div className="flex justify-center items-center">
         <ClimbingBoxLoader color="#593b99" />
       </div>
+      <FixedBottomContent>
+        {definitions && definitions.length > 0 && (
+          <h2 className="italic text-md xl:text-2xl px-4">
+            {submittedWord.toUpperCase()}: {definitions[definitionIdx].substr(0, 190)}...
+          </h2>
+        )}
+        <p className=" mt-6 animate-bounce font-sans italic">Waiting for other players...</p>
+      </FixedBottomContent>
       {/*      <div className="spacer h-10 xl:h-20"></div>*/}
-      {definitions && definitions.length > 0 && (
-        <h2 className="italic text-md xl:text-2xl px-4">
-          {submittedWord.toUpperCase()}: {definitions[definitionIdx].substr(0, 190)}...
-        </h2>
-      )}
-      <p className=" mt-6 animate-bounce font-sans italic">Waiting for other players...</p>
     </motion.div>
   );
 };
