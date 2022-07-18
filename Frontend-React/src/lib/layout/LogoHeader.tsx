145e91c7968ea9a88db8b521609edadd3bc1a270
Pål Nødseth <p@nodseth.no>
2022-07-19 00:16:29 +0200

    add font-awesome

--- Frontend-React/src/lib/layout/LogoHeader.tsx
+++ Frontend-React/src/lib/layout/LogoHeader.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { playerContext } from "$lib/contexts/PlayerContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 interface ILogoHeader {
   headerSize: "small" | "large";
@@ -12,9 +14,12 @@ export const LogoHeader: React.FC<ILogoHeader> = ({ headerSize }) => {
       {headerSize === "large" ? (
         <h1 className="text-6xl text-center font-kawoord text-white">Kawoord</h1>
       ) : (
-        <div className="flex justify-between items-center font-kawoord pr-8">
+        <div className="flex justify-between items-center font-kawoord pr-4">
           <h1 className="text-xl text-left font-kawoord text-white">Kawoord</h1>
-          <div>{player.name}</div>
+          <div className="flex items-center gap-1">
+            <div>{player.name}</div>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
         </div>
       )}
     </div>
