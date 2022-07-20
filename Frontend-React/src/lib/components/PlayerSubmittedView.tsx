0d541f0245acd50d3eb451553bb44af0ca0f068c
Pål Nødseth <p@nodseth.no>
2022-07-20 08:52:06 +0200

    Fix PlayerSubmittedView.tsx

--- Frontend-React/src/lib/components/PlayerSubmittedView.tsx
+++ Frontend-React/src/lib/components/PlayerSubmittedView.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import useWordDefinition from "$lib/hooks/useWordDefinition";
-import FixedBottomContent from "$lib/layout/FixedBottomContent";
 
 interface PlayerHasSubmittedProps {
   submittedWord: string;
@@ -24,22 +23,25 @@ export const PlayerSubmittedView = ({ submittedWord }: PlayerHasSubmittedProps) 
     };
   }, [definitions]);
   return (
-    <motion.div animate={{ opacity: [0, 1] }} transition={{ duration: 0.4, type: "spring" }}>
-      <h2 className="font-kawoord text-2xl">Great job!</h2>
-      <h2 className="font-kawoord text-xl">You submitted: {submittedWord.toUpperCase()}</h2>
-      <div className="spacer sm:8 xl:h-20"></div>
-      {/* <div className="flex justify-center items-center">
-        <ClimbingBoxLoader color="#593b99" />
-      </div>*/}
-      <FixedBottomContent>
+    <motion.div
+      animate={{ opacity: [0, 1] }}
+      transition={{ duration: 0.4, type: "spring" }}
+      className="flex flex-col h-full"
+    >
+      <div className="flex-grow-0">
+        <h2 className=" text-xl">You submitted: </h2>
+        <div className="spacer h-2"></div>
+        <h2 className="font-kawoord text-6xl text-kawoordLilla">{submittedWord.toUpperCase()}</h2>
+        <div className="spacer h-8  lg:h-20"></div>
+      </div>
+      <div className="definition flex-1">
         {definitions && definitions.length > 0 && (
-          <h2 className="italic text-md xl:text-2xl px-4">
-            {submittedWord.toUpperCase()}: {definitions[definitionIdx].substr(0, 190)}...
-          </h2>
+          <h2 className="italic text-md lg:text-2xl px-4 font-sans">{definitions[definitionIdx].substr(0, 190)}...</h2>
         )}
+      </div>
+      <div className="waiting">
         <p className=" mt-6 animate-bounce font-sans italic">Waiting for other players...</p>
-      </FixedBottomContent>
-      {/*      <div className="spacer h-10 xl:h-20"></div>*/}
+      </div>
     </motion.div>
   );
 };
