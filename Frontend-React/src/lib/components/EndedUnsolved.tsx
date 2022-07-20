48cb0a2c8d8d7ac483a750d81666dc122236c598
Pål Nødseth <p@nodseth.no>
2022-07-20 09:08:26 +0200

    Fix Unsolved

--- Frontend-React/src/lib/components/EndedUnsolved.tsx
+++ Frontend-React/src/lib/components/EndedUnsolved.tsx
@@ -13,10 +13,10 @@ export const EndedUnsolved: React.FC<EndedUnsolvedProps> = ({ solution }: EndedU
   return (
     <motion.div animate={{ opacity: [0, 1] }} transition={{ duration: 0.4, type: "spring" }} className="text-center">
       <h1 className="font-kawoord text-4xl my-8">Game ended</h1>
-      <h3 className="font-kawoord text-xl mb-8">Unsolved 😞</h3>
-      <p className="mb-4">The correct word was...</p>
-      <p className="font-kawoord text-2xl">{solution?.toUpperCase()}</p>
-      <div className="spacer h-8"></div>
+      <h3 className="font-kawoord text-2xl mb-8 text-kawoordWhite">Unsolved 😞</h3>
+      <p className="mb-1 italic">The correct word was...</p>
+      <p className="font-kawoord text-6xl text-kawoordYellow">{solution?.toUpperCase()}</p>
+      <div className="spacer h-20"></div>
       <Button onClick={() => gameService.clearGame()}>Play again?</Button>
     </motion.div>
   );
